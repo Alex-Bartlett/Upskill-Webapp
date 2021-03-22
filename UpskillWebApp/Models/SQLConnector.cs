075676c910b6400162668fcb665324fc0239bf86
Alex Bartlett <ab1425@canterbury.ac.uk>
2021-03-22 10:04:14 +0000

    Added SQL connector class.
    Not complete yet, but will have functionality to connect to the server.

---
+++ UpskillWebApp/Models/SQLConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace UpskillWebApp.Models
+{
+	public class SQLConnector
+	{
+		string connectionString = @"Server = tcp:upskilljobmgmt.database.windows.net,1433; Initial Catalog = Upskill; Persist Security Info = False; User ID = AdminAccount; Password =@!llCQkr35Pb; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+
+
+	}
+}
