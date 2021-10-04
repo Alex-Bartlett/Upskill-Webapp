dda09be23712b05e725701438d8ba5c5c8185f48
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-10-04 19:45:41 +0100

    Add dashboard tables

--- UpskillWebapp/Upskill/Pages/Index.cshtml.cs
+++ UpskillWebapp/Upskill/Pages/Index.cshtml.cs
@@ -5,21 +5,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Upskill.Models;
+using Upskill.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Upskill.Pages
 {
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+		private readonly UpskillContext _context;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		public IndexModel(ILogger<IndexModel> logger, UpskillContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
-		public void OnGet()
-		{
+		public IList<Job> Jobs { get; set; }
+		public IList<Customer> Customers { get; set; }
 
+		public async Task OnGetAsync()
+		{
+			IQueryable<Job> jobsQuery = from j in _context.Jobs
+										select j;
+			jobsQuery = jobsQuery.OrderByDescending(j => j.CreationTime)
+				.Take(5);
+
+			Jobs = await jobsQuery
+				.Include(j => j.Customer)
+				.AsNoTracking()
+				.ToListAsync();
+
+			IQueryable<Customer> customersQuery = from c in _context.Customers
+												  select c;
+			customersQuery = customersQuery.OrderByDescending(c => c.Jobs.Count())
+				.Take(5);
+
+			Customers = await customersQuery
+				.AsNoTracking()
+				.ToListAsync();
 		}
 	}
 }
