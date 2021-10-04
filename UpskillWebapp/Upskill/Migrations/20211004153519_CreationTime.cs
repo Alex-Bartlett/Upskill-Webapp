dbb698f14faf5358ce750906923ccb829614fb7f
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-10-04 17:19:24 +0100

    Add CreationTime (Last updated) to all main tables

---
+++ UpskillWebapp/Upskill/Migrations/20211004153519_CreationTime.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Upskill.Migrations
+{
+    public partial class CreationTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "StaffMember",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "StaffJob",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "Job",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "Customer",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "StaffMember");
+
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "StaffJob");
+
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "Job");
+
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "Customer");
+        }
+    }
+}
